91ef67a17ed742e84fa4aa492139a861842f598d
IanHalfen <68925865+IanHalfen@users.noreply.github.com>
2023-11-26 14:37:19 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>One Piece - 47</h2>
+      <video width="1080px" height="720px" controls>
+        <source src='https://dgl6p95w1mefo.cloudfront.net/onepiece_47.mkv' type='video/mp4'></source>
+      </video>
+    </div>
+  );
+}
+
+export default App;
